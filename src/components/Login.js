a92c5f416a5b2457299adfcd20dcc4cffd54cbbc
angesom hailu <angesomhailu1414@gmail.com>
2025-04-01 13:11:59 +0300

    another commit

---
+++ src/components/Login.js
@@ -0,0 +1,14 @@
+
+import React from "react";
+import Logo from "./Logo";
+const Login = () => {
+  return (
+    <div>
+         <div className="login_bg_gradient bg-cover h-screen grid place-items-center">
+         <Logo/>
+    </div>
+    </div>
+  );
+};
+
+export default Login;
